38bb5b4192f2c7170249fe4835ddc632cc50521b
David <imdavidf@gmail.com>
2018-11-16 00:02:28 -0800

    Add loader

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 
 import DisplayBoard from './component/displayBoard';
 import getNewAirData from './actions/index';
+import Loader from './component/loader';
 import logo from './logo.svg';
 import './App.css';
 
@@ -44,26 +45,32 @@ class App extends Component {
     }
   }
   render() {
-    if (this.state.location) {
-      const location = this.state.location.coords;
-      // console.log(location)
-      return (
-        <div>
-          <DisplayBoard getCountingFinished={this.getCountingFinished} />
-          {this.state.counting  
-          ? <button onClick={this.checkNowOnClick} className="check-button diable" disabled>Checking</button>
-          : <button onClick={this.checkNowOnClick} className="check-button">Update</button>
-          }
-          <div id="bg-healthy"></div>
-          <div id="bg-moderate"></div>
-          <div id="bg-sensitive"></div>
-          <div id="bg-unhealthy"></div>
-          <div id="bg-danger"></div>
-          <div id="bg-hazardous"></div>
-        </div>
-      );
-    }
-    return (<h1 className="loading">Loading</h1>);
+      if (this.state.location) {
+        const location = this.state.location.coords;
+        // console.log(location)
+        return (
+          <div>
+            <DisplayBoard getCountingFinished={this.getCountingFinished} />
+            {this.state.counting  
+            ? <button onClick={this.checkNowOnClick} className="check-button diable" disabled>Checking</button>
+            : <button onClick={this.checkNowOnClick} className="check-button">Update</button>
+            }
+            <div id="bg-healthy"></div>
+            <div id="bg-moderate"></div>
+            <div id="bg-sensitive"></div>
+            <div id="bg-unhealthy"></div>
+            <div id="bg-danger"></div>
+            <div id="bg-hazardous"></div>
+          </div>
+        );
+      }
+    return (
+      <div className="vertical-center"> 
+        <div className="loader-container">
+          <Loader />
+      </div>
+      </div>
+    );
   }
 }
 
---
+++ src/component/loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="loader">
+            <div className="particle green"></div>
+            <div className="particle orange"></div>
+            <div className="particle red"></div>
+            <div className="particle purple"></div>
+        </div>
+    );
+}
