43800f8d14b26c594b72b37be1cf1975ae7a66d2
David <imdavidf@gmail.com>
2018-11-15 21:09:27 -0800

    https

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ const getNewAirData = async (location) => {
   const lat = location.latitude;
   const lng = location.longitude;
   const airData = axios.get(`https://api.waqi.info/feed/geo:${lat};${lng}/?token=${API_KEY}`);
-  const airNowData = await axios.get(`http://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=37.936&longitude=-122.0262&distance=25&API_KEY=CFE97C56-8C64-49C2-BA8C-10652F3442C2`)
+  const airNowData = await axios.get(`https://www.airnowapi.org/aq/observation/latLong/current/?format=application/json&latitude=37.936&longitude=-122.0262&distance=25&API_KEY=CFE97C56-8C64-49C2-BA8C-10652F3442C2`)
   console.log(airNowData)
   // console.log(airData)
   return {
